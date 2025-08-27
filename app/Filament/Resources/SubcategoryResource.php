735db390f9bbe4b988746a00a21f4a0776f6e5ce
Sayed Mustafa Qarooni <140971464+mqarooni@users.noreply.github.com>
2025-08-27 11:33:23 +0300

    add the Delete Action

--- app/Filament/Resources/SubcategoryResource.php
+++ app/Filament/Resources/SubcategoryResource.php
@@ -59,6 +59,7 @@ class SubcategoryResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
